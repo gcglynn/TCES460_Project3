83020bc6a7ab86f99175139aea4a87a51e62cbaa
Stefan <stefant@uw.edu>
2016-02-26 22:51:59 +0000

    Update video_client.py to continuously send images

--- video_client.py
+++ video_client.py
@@ -11,7 +11,7 @@ if not cam.isOpened():
 
 ret, frame = cam.read()
 
-TCP_IP = '10.16.3.253'
+TCP_IP = '10.16.10.123'
 PORT = 5001
 
 print("Connecting")
@@ -19,15 +19,26 @@ sock = socket.socket()
 sock.connect((TCP_IP, PORT))
 print("Connected")
 
-
-
 encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
-result, imgencode = cv2.imencode('.jpg', frame, encode_param)
-data = numpy.array(imgencode)
-stringData = data.tostring()
 
-sock.send( str(len(stringData)).ljust(16));
-sock.send( stringData);
+frameCount = 0
+try:
+    while True:
+        ret, frame = cam.read()
+
+        result, imgencode = cv2.imencode('.jpg', frame, encode_param)
+        data = numpy.array(imgencode)
+        stringData = data.tostring()
+
+        print("Sending frame " + str(frameCount))
+        frameCount = frameCount + 1
+
+        sock.send( str(len(stringData)).ljust(16));
+        sock.send( stringData);
+except KeyboardInterrupt:
+    pass
+
+sock.shutdown(2)
 sock.close()
-
+print("Exiting")
 
