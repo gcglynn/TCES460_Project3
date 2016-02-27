8ff07ddfb594b0ab658e85826df3be8093a9dd3d
Stefan <stefant@uw.edu>
2016-02-27 00:17:10 +0000

    Add detection for a blue ball
    Detects a blue area and calculates the coordinates.

--- video_client.py
+++ video_client.py
@@ -9,8 +9,6 @@ if not cam.isOpened():
     print("Camera not open")
     exit()
 
-ret, frame = cam.read()
-
 TCP_IP = '10.16.10.123'
 PORT = 5001
 
@@ -26,7 +24,34 @@ try:
     while True:
         ret, frame = cam.read()
 
-        result, imgencode = cv2.imencode('.jpg', frame, encode_param)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+#        [red, green, blue] = cv2.split(frame)
+        [hue, sat, val] = cv2.split(hsv)
+        blue = cv2.inRange(hue, 75, 130)
+        saturated = cv2.inRange(sat, 127, 255)
+        ball = cv2.bitwise_and(blue, saturated)
+#        print(cv2.moments(ball))
+        moments = cv2.moments(ball)
+        x = int(moments['m10'] / moments['m00'])
+        y = int(moments['m01'] / moments['m00'])
+        print("x: " + str(x))
+        print("y: " + str(y))
+
+        outputFrame = frame
+        cv2.circle(outputFrame, (x, y), 16, (255, 255, 255), -1)
+#        outputFrame = ball
+#        ret, val = cv2.threshold(val, 127, 255, cv2.THRESH_BINARY)
+#        outputFrame = cv2.merge([hue, sat, val])       
+#        outputFrame = cv2.cvtColor(outputFrame, cv2.COLOR_HSV2BGR)
+#        ret, red = cv2.threshold(red, 127, 255, cv2.THRESH_BINARY_INV)
+#        ret, green = cv2.threshold(green, 127, 255, cv2.THRESH_BINARY_INV)
+#        ret, blue = cv2.threshold(blue, 127, 255, cv2.THRESH_BINARY)
+
+#        outputFrame = cv2.merge([red, green, blue])
+        #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        #ret, frame = cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY)
+
+        result, imgencode = cv2.imencode('.jpg', outputFrame, encode_param)
         data = numpy.array(imgencode)
         stringData = data.tostring()
 
