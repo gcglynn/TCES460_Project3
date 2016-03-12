4ce99e10c9b94a5a2973e26234678a0b9f2f57cc
Stefan <stefant@uw.edu>
2016-03-12 00:57:26 +0000

    Tweaked video_client.py to work well with the maze

--- video_client.py
+++ video_client.py
@@ -16,6 +16,8 @@ BALL_MIN_HUE = 75
 BALL_MAX_HUE = 130
 BALL_MIN_SAT = 127
 BALL_MAX_SAT = 255
+BALL_MIN_VAL = 32
+BALL_MAX_VAL = 240
 PRINT_BALL_COORDS = True
 MARK_BALL = True
 BALL_COLOR = (255, 0, 0)
@@ -25,7 +27,7 @@ PLAT_MAX_VAL = 255
 PLAT_MIN_SAT = 0
 PLAT_MAX_SAT = 63
 PRINT_PLAT_COORDS = False
-MARK_PLAT = True
+MARK_PLAT = False
 PLAT_COLOR = (0, 0, 255)
 
 ERODE_KERNEL_SIZE = 5
@@ -103,35 +105,39 @@ try:
         [hue, sat, val] = cv2.split(hsv)
         timepoint("ToHSV")
         
+        kernel = numpy.ones((ERODE_KERNEL_SIZE, ERODE_KERNEL_SIZE),numpy.uint8)
+
         #Detect the blue ball
         blue = cv2.inRange(hue, BALL_MIN_HUE, BALL_MAX_HUE)
         saturated = cv2.inRange(sat, BALL_MIN_SAT, BALL_MAX_SAT)
+        mediumLightness = cv2.inRange(val, BALL_MIN_VAL, BALL_MAX_VAL)
         ball = cv2.bitwise_and(blue, saturated)
+        ball = cv2.bitwise_and(ball, mediumLightness)
+        ball = cv2.erode(ball, kernel, iterations = 1)
         timepoint("FindBall")
         
-        # Detect the white-ish platform
-        light = cv2.inRange(val, PLAT_MIN_VAL, PLAT_MAX_VAL)
-        unsaturated = cv2.inRange(sat, PLAT_MIN_SAT, PLAT_MAX_SAT)
-        platform = cv2.bitwise_and(light, unsaturated)
-        kernel = numpy.ones((ERODE_KERNEL_SIZE, ERODE_KERNEL_SIZE),numpy.uint8)
-        platform = cv2.erode(platform, kernel, iterations = 1)
+#        # Detect the white-ish platform
+#        light = cv2.inRange(val, PLAT_MIN_VAL, PLAT_MAX_VAL)
+#        unsaturated = cv2.inRange(sat, PLAT_MIN_SAT, PLAT_MAX_SAT)
+#        platform = cv2.bitwise_and(light, unsaturated)
+#        platform = cv2.erode(platform, kernel, iterations = 1)
 
-        contours, hierarchy = cv2.findContours(platform, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        if len(contours) > 0:
-            x,y,w,h = cv2.boundingRect(contours[0])
-        timepoint("FindPlatform")
-        if PRINT_PLAT_COORDS:
-            print("x: " + str(x) + " y: " + str(y) + " w: " + str(w) + " h: " + str(h))
-            timepoint("PrintPlatform")
+#        contours, hierarchy = cv2.findContours(platform, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#        if len(contours) > 0:
+#            x,y,w,h = cv2.boundingRect(contours[0])
+#        timepoint("FindPlatform")
+#        if PRINT_PLAT_COORDS:
+#            print("x: " + str(x) + " y: " + str(y) + " w: " + str(w) + " h: " + str(h))
+#            timepoint("PrintPlatform")
 
         moments = cv2.moments(ball)
-        outputFrame = frame
-#        outputFrame = ball
+#        outputFrame = frame
+#        outputFrame = cv2.bitwise_and(frame, frame, mask=ball)
 #        outputFrame = platform
 
-        if MARK_PLAT:
-            cv2.rectangle(outputFrame, (x,y), (x+w,y+h), PLAT_COLOR, 2)
-            timepoint("MarkPlatform")
+#        if MARK_PLAT:
+#            cv2.rectangle(outputFrame, (x,y), (x+w,y+h), PLAT_COLOR, 2)
+#            timepoint("MarkPlatform")
 
         if moments['m00'] != 0:
             xRaw = int(moments['m10'] / moments['m00'])
@@ -153,7 +159,7 @@ try:
                 timepoint("MarkBall")
 
         sendFrame.number = frameCount
-        sendFrame.frame = frame
+        sendFrame.frame = outputFrame
 
         if PRINT_TIMES:
             print(times)
