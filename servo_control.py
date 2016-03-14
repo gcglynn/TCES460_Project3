f1fcc3d7b70747fc5e1c850cc596c0ac5bac070a
dloulias <demyloulias@gmail.com>
2016-03-14 21:17:26 +0000

    update to initialization of servos

--- servo_control.py
+++ servo_control.py
@@ -1,49 +1,61 @@
 import mraa
 import time
 
-short_home=1000
-short_max=1700
-short_min=500
-short_pin=9
-short_servo=short_pin
+#short side = y
+#long side = x
 
-long_home=900
-long_max=1500
-long_min=500
-long_pin=5
-long_servo=long_pin
+y_home=1000
+y_max=1700
+y_min=400
+y_pin=9
 
-def move_to_side(servo, degree):
-	servo=mraa.Pwm(servo)
-	servo.period_ms(20)
-	servo.write(.2)
-	servo.pulsewidth_us(degree)
-	servo.enable(True)
-	time.sleep(.5)
+x_home=900
+x_max=1500
+x_min=500
+x_pin=5
 
-def move_to_neutral():
-	move_to_side(short_servo, short_home)
-        move_to_side(long_servo, long_home)
-        time.sleep(2)
+x_servo=0
+y_servo=0
+
+def init_servo(pin, home):
+	servo=mraa.Pwm(pin)
+        servo.period_ms(20)
+        servo.write(.2)
+        servo.pulsewidth_us(home)
+        servo.enable(True)
+        return servo
+
+def tilt(servo, position):
+	servo.pulsewidth_us(position)
+
+def tilt_neutral():
+	tilt(y_servo, y_home)
+        tilt(x_servo, x_home)
 	
 def init_axis():
-	move_to_side(short_servo, short_max)
-	move_to_side(long_servo, long_max)
-	time.sleep(2)
+	tilt(y_servo, y_max)
+	tilt(x_servo, x_max)
+	time.sleep(3)
 	
-        move_to_side(short_servo, short_min)
-        move_to_side(long_servo, long_max)
-        time.sleep(2)
+#        tilt(short_servo, short_min)
+#        tilt(long_servo, long_max)
+#        time.sleep(2)
 
-        move_to_side(short_servo, short_min)
-        move_to_side(long_servo, long_min)
-        time.sleep(2)
+        tilt(y_servo, y_min)
+        tilt(x_servo, x_min)
+        time.sleep(3)
 
-        move_to_side(short_servo, short_max)
-        move_to_side(long_servo, long_min)
-        time.sleep(2)
+#        tilt(short_servo, short_max)
+#        tilt(long_servo, long_min)
+#        time.sleep(2)
 
-	move_to_neutral()
 
-move_to_neutral()
+y_servo=init_servo(y_pin, y_home)
+x_servo=init_servo(x_pin, x_home)
+
+tilt_neutral()
+time.sleep(2)
 init_axis()
+tilt_neutral()
+while(True):
+	pass
